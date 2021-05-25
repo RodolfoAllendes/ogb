60a37e67518169e4d4d1a8ef19a8a764caa827e9
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2021-05-25 18:13:19 +0900

    Trying to filter some of the dataset for the definition of training and evaluation datasets.

--- examples/lsc/wikikg90m/dgl-ke-ogb-lsc/python/dglke/simpleRun.py
+++ examples/lsc/wikikg90m/dgl-ke-ogb-lsc/python/dglke/simpleRun.py
@@ -4,7 +4,7 @@ import os
 import logging
 import time
 
-# from .dataloader import EvalDataset, TrainDataset, NewBidirectionalOneShotIterator
+from dataloader import EvalDataset, TrainDataset, NewBidirectionalOneShotIterator
 # from .dataloader import get_dataset
 from dataloader import RandomPartition
 
@@ -27,7 +27,98 @@ import dgl.backend as F
 import dgl
 from dgl.base import NID, EID
 
+def CustomPartition(edges, n, p, has_importance=False):
+    """Based on BalancedPartition and RandomPartition (from sampler), we will 
+    extract only part of the original dataset to each partition.
+    
+    Algorithm:
+    For r in relations:
+      Find partition with fewest edges
+      if r.size() > num_of empty_slot
+         put edges of r into this partition to fill the partition,
+         find next partition with fewest edges to put r in.
+      else
+         put edges of r into this partition.
+
+    Returns
+    -------
+    List of np.array
+        Edges of each partition
+    """
+    # split the triplets into heads, relation and tails
+    if has_importance:
+        heads, rels, tails, e_impts = edges
+    else:
+        heads, rels, tails = edges
+
+    print('Custom partition - Total Edges: {}, Using (aprox): {} into {} parts'.format(len(heads),np.ceil(len(heads)*p),n))
+    # name and count of each type of edge,
+    # sorted from largest to smallest 
+    uniq, cnts = np.unique(rels, return_counts=True)
+    idx = np.flip(np.argsort(cnts))
+    cnts = cnts[idx]
+    uniq = uniq[idx]
+
+    # adjusted count for the percentage of extraction
+    p_cnts = np.ceil(cnts*p).astype(int)
+    
+    # array of indices for each part in the division of the triplets
+    parts = []
+    for i in range(n):
+        parts.append([])
+    
+    #for each type of relation (edge)
+    for i, type_rel in enumerate(uniq):
+        # total number of triplets of the current type to be added to the different 
+        # parts 
+        cnt = p_cnts[i]
+        x = cnt//n if cnt >= n else 1 # number of elements for each part
+        
+        # a mask for the triplets whose relation type matches type_rel
+        t_idx = rels == type_rel
+        a_idx = np.arange(start=0, stop=len(rels))
+        # now t_idx is a list of indices of triplets whose relation matches type_rel
+        # (we used the mask to filter out the indices)
+        t_idx = a_idx[t_idx]
+        # in order to make a random selection of the indices, we permutate them
+        t_idx = np.random.permutation(t_idx)
+        print('for type {} we have {} items, will use {}'.format(i,len(t_idx),cnt), end='\r', flush=True)
+        t_idx = t_idx[:cnt]
+        # for each part 
+        off = 0
+        for j in range(n):
+            
+            # add indices for each part (only if there are indices left to add)
+            if off+x < cnt: 
+                parts[j].append(t_idx[off:off+x])
+                off += x 
+            # if not enough (but still some) add it
+            elif off < cnt:
+                parts[j].append(t_idx[off:])
+                off += x
+            else:
+                pass
+    # at this point, all parts should already have the indices they need, but 
+    # they are ordered sequentially from largest to smallest number of edge_type
+    # so, shuffle the indices
+    # first convert the parts to numpy ndarrays
+    for i, part in enumerate(parts):
+        parts[i] = np.array(part, dtype=np.int64)
+        print('Part[{}] has {} elements: {}'.format(i,len(parts[i]), parts[i]))
+    # put them all together
+    shuffle_idx = np.concatenate(parts)
+    # extract only the parts that were randomly selected
+    heads[:] = heads[shuffle_idx]
+    rels[:] = rels[shuffle_idx]
+    tails[:] = tails[shuffle_idx]
+    if has_importance:
+        e_impts[:] = e_impts[shuffle_idx]
+
+    # and return the results
+    return parts
+
 class SimpleRunner(object):
+    
     def __init__(self):
 
         # Step 1 of the process LOAD THE DATASET
@@ -89,6 +180,12 @@ class SimpleRunner(object):
 
 # args.soft_rel_part = args.mix_cpu_gpu and args.rel_part
 
+# # if there is no cross partition relaiton, we fall back to strict_rel_part
+# args.strict_rel_part = args.mix_cpu_gpu and (train_data.cross_part == False)
+        self.num_workers = 8 # fix num_worker to 8
+
+    def build_training_dataset(self):
+
         # construct the training Graph
         print ("To build training dataset")       
         t1 = time.time()
@@ -97,17 +194,20 @@ class SimpleRunner(object):
         coo = sp.sparse.coo_matrix((np.ones(len(src)), (src, dst)), shape=[self.n_entities, self.n_entities])
         self.train_data = dgl.DGLGraph(coo, readonly=True, multigraph=True, sort_csr=True)
         self.train_data.edata['tid'] = F.tensor(etype_id, F.int64)
-        self.train_data.edge_parts = RandomPartition(edges=self.train, n=self.num_proc, has_importance=self.has_edge_importance)
+        self.train_data.edge_parts = CustomPartition(edges=self.train, n=self.num_proc, p=0.1, has_importance=self.has_edge_importance)
+        # self.train_data.edge_parts = RandomPartition(edges=self.train, n=self.num_proc, has_importance=self.has_edge_importance)
+        print(self.train_data.edge_parts[0])
+        print(len(self.train_data.edge_parts[0]))
         self.train_data.cross_part = True
         print ("Training dataset built, it takes %d seconds"%(time.time()-t1))
 
 
-# # if there is no cross partition relaiton, we fall back to strict_rel_part
-# args.strict_rel_part = args.mix_cpu_gpu and (train_data.cross_part == False)
-        self.num_workers = 8 # fix num_worker to 8
+
 # set_logger(args)
 # with open(os.path.join(args.save_path, args.encoder_model_name), 'w') as f:
 #     f.write(args.encoder_model_name)
+
+    def generate_samplers(self):
 
         # Generate train samplers
         train_samplers = []
@@ -130,7 +230,7 @@ class SimpleRunner(object):
 
             train_sampler_tail = dgl.contrib.sampling.EdgeSampler(
                 self.train_data,
-                seed_edges = F.tensor(self.edge_parts[i]),   
+                seed_edges = F.tensor(self.train_data.edge_parts[i]),   
                 batch_size = self.batch_size,
                 neg_sample_size = self.neg_sample_size,
                 chunk_size = self.neg_sample_size,
@@ -144,27 +244,25 @@ class SimpleRunner(object):
             print(train_sampler_head)
             print(train_sampler_tail)
 
-        #     train_samplers.append(NewBidirectionalOneShotIterator(train_sampler_head, train_sampler_tail,
-        #                                                             args.neg_sample_size, args.neg_sample_size,
-        #                                                             True, dataset.n_entities,
-        #                                                             args.has_edge_importance))
-        #     print("Training sampler for proc %d created, it takes %s seconds"%(i, time.time()-t1))
+            train_samplers.append(NewBidirectionalOneShotIterator(
+                dataloader_head = train_sampler_head, 
+                dataloader_tail = train_sampler_tail,
+                neg_chunk_size = self.neg_sample_size,
+                neg_sample_size = self.neg_sample_size,
+                is_chunked = True,
+                num_nodes = self.n_entities,
+                has_edge_importance = self.has_edge_importance,
+            ))
+            print("Training sampler for proc {} created, it takes {} seconds".format(i, time.time()-t1))
 
-        # train_sampler = NewBidirectionalOneShotIterator(train_sampler_head, train_sampler_tail,
-        #                                                 args.neg_sample_size, args.neg_sample_size,
-        #                                                 True, dataset.n_entities,
-        #                                                 self.has_edge_importance)
+    def build_evaluation_dataset(self):
 
-
-# if args.valid or args.test:
-#     if len(args.gpu) > 1:
-#         args.num_test_proc = args.num_proc if args.num_proc < len(args.gpu) else len(args.gpu)
-#     else:
-#         args.num_test_proc = args.num_proc
-#     print("To create eval_dataset")
-#     t1 = time.time()
-#     eval_dataset = EvalDataset(dataset, args)
-#     print("eval_dataset created, it takes %d seconds" % (time.time() - t1))
+        # Create the Evaluation Datset
+        self.num_test_proc = self.num_proc
+        print("To create eval_dataset")
+        t1 = time.time()
+        eval_dataset = EvalDataset(dataset, args)
+        print("eval_dataset created, it takes %d seconds" % (time.time() - t1))
 
 # if args.valid:
 #     if args.num_proc > 1:
@@ -276,9 +374,13 @@ class SimpleRunner(object):
 #     train(args, model, train_sampler, valid_samplers, test_samplers, rel_parts=rel_parts)
 
 # print('training takes {} seconds'.format(time.time() - start))
-
-def main():
-    sr = SimpleRunner()
+   
 
 if __name__ == '__main__':
-    main()
+    
+    sr = SimpleRunner()
+
+    sr.build_training_dataset()
+    # sr.generate_samplers()
+
+    # sr.build_evaluation_dataset()
